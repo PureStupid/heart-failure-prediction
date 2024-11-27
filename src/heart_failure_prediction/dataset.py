66dce516d4f861d9d7a2f2af9712540de07e492b
Pure <111236541+PureStupid@users.noreply.github.com>
2024-11-27 21:55:18 +0800

    Add logging to data download and loading functions

--- src/heart_failure_prediction/dataset.py
+++ src/heart_failure_prediction/dataset.py
@@ -5,6 +5,7 @@ This module contains functions to download data.
 
 import pandas as pd
 from dvc.api import DVCFileSystem
+from loguru import logger
 
 from heart_failure_prediction.config import DVC_REMOTE_CONFIG
 
@@ -22,18 +23,19 @@ def download_raw_data() -> None:
     The raw data (`heart.csv`) is downloaded from the remote storage to the
     local storage.
     """
+    logger.info("Downloading raw data.")
     file_system.get("data/raw/heart.csv", "data/raw/heart.csv")
+    logger.success("Raw data downloaded.")
 
 
 def load_raw_data() -> pd.DataFrame:
-    """Load the raw data (`heart.csv`), downloading it if it doesn't exist.
+    """Load the raw data (`heart.csv`).
 
     Returns
     -------
         pd.DataFrame
             The raw data.
     """
-    if not file_system.exists("data/raw/heart.csv"):
-        download_raw_data()
+    logger.info("Loading raw data.")
     with file_system.open("data/raw/heart.csv") as file:
         return pd.read_csv(file)
